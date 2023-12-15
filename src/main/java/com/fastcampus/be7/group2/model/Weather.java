fdf32b7bf7633db9715e5995069b845641b9702b
kimkyungho <dev_kimkyungho@kakao.com>
2023-12-15 23:35:36 +0900

    base class

---
+++ src/main/java/com/fastcampus/be7/group2/model/Weather.java
@@ -0,0 +1,7 @@
+package com.fastcampus.be7.group2.model;
+
+/**
+ * Created by KimKyungHo on 2023-12-15(015)
+ */
+public class Weather {
+}
---
+++ src/main/java/com/fastcampus/be7/group2/service/WeatherService.java
@@ -0,0 +1,7 @@
+package com.fastcampus.be7.group2.service;
+
+/**
+ * Created by KimKyungHo on 2023-12-15(015)
+ */
+public class WeatherService {
+}
